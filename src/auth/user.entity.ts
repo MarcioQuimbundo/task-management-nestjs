8a009da018f1328301859024e18d9169c9534d1a
MarcioQuimbundo <marcioquimbundo@hotmail.com>
2019-11-27 19:33:49 +0100

    Security Hashing Passwords & Using Salts

--- src/auth/user.entity.ts
+++ src/auth/user.entity.ts
@@ -1,14 +1,23 @@
-import { BaseEntity, Entity, PrimaryGeneratedColumn, Column, Unique } from "typeorm";
+import {
+  BaseEntity,
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  Unique,
+} from 'typeorm';
 
 @Entity()
 @Unique(['username'])
 export class User extends BaseEntity {
-    @PrimaryGeneratedColumn()
-    id: number;
+  @PrimaryGeneratedColumn()
+  id: number;
 
-    @Column()
-    username: string;
-    
-    @Column()
-    password: string;
+  @Column()
+  username: string;
+
+  @Column()
+  password: string;
+
+  @Column()
+  salt: string;
 }
--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,7 +1,11 @@
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
+import * as bcrypt from 'bcrypt';
 import { User } from './user.entity';
 import { Repository, EntityRepository } from 'typeorm';
-import { ConflictException, InternalServerErrorException } from '@nestjs/common';
+import {
+  ConflictException,
+  InternalServerErrorException,
+} from '@nestjs/common';
 
 @EntityRepository(User)
 export class UserRepository extends Repository<User> {
@@ -10,17 +14,22 @@ export class UserRepository extends Repository<User> {
 
     const user = new User();
     user.username = username;
-    user.password = password;
+    user.salt = await bcrypt.genSalt();
+    user.password = await this.hashPassword(password, user.salt);
+
     try {
       await user.save();
     } catch (error) {
-      if (error.code === "23505") {
+      if (error.code === '23505') {
         //duplicate username
         throw new ConflictException('Username already exists');
       } else {
-          throw new InternalServerErrorException();
-          
+        throw new InternalServerErrorException();
       }
     }
+  }
+
+  private async hashPassword(password: string, salt: string): Promise<string> {
+    return bcrypt.hash(password, salt);
   }
 }
